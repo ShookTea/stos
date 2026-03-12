44be99629d0e69b7e276128fb76da8245989ab3f
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-12 11:43:48 +0100

    Registering timeouts in PIT
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/arch/i386/drivers/pit.c
+++ src/arch/i386/drivers/pit.c
@@ -11,20 +11,54 @@
 #define PIT_CHANNEL_0_DATA_PORT 0x40
 // 1 kHz
 #define PIT_FREQUENCY 1000
+#define PIT_TIMEOUT_CALLBACK_SIZE 32
 
 static uint32_t tick = 0;
 static uint32_t second = 0;
 static bool initialized = false;
 
+static struct timeout_entries {
+    uint32_t expire_tick;
+    timeout_callback_t callback;
+    void* data;
+    bool active;
+} timeouts[PIT_TIMEOUT_CALLBACK_SIZE];
+
 static void pit_timer_callback()
 {
     tick++;
-    if (tick == PIT_FREQUENCY) {
-        tick = 0;
+    for (int i = 0; i < PIT_TIMEOUT_CALLBACK_SIZE; i++) {
+        if (timeouts[i].active && tick >= timeouts[i].expire_tick) {
+            timeouts[i].active = false;
+            timeouts[i].callback(timeouts[i].data);
+        }
+    }
+    if (tick % PIT_FREQUENCY == 0) {
         second++;
         printf("Second %d\n", second);
     }
+}
 
+int pit_register_timeout(
+    uint32_t ms,
+    timeout_callback_t callback,
+    void* data
+) {
+    int id = -1;
+    for (int i = 0; i < PIT_TIMEOUT_CALLBACK_SIZE; i++) {
+        if (!timeouts[i].active) {
+            id = i;
+            break;
+        }
+    }
+    if (id == -1) {
+        return id;
+    }
+    timeouts[id].expire_tick = tick + ms;
+    timeouts[id].callback = callback;
+    timeouts[id].data = data;
+    timeouts[id].active = true;
+    return id;
 }
 
 void pit_init()
--- src/include/kernel/drivers/pit.h
+++ src/include/kernel/drivers/pit.h
@@ -1,5 +1,26 @@
 #ifndef KERNEL_DRIVERS_PIT_H
 #define KERNEL_DRIVERS_PIT_H
 
+#include <stdint.h>
+
+// Timeout callback function
+typedef void (*timeout_callback_t)(void* data);
+
 void pit_init();
+
+/**
+ * Register a new timeout that will be called after a given time, with selected
+ * data. Returns the ID of the timeout, that can be referenced in the
+ * pit_cancel_timeout function, or -1 if there are no more timeouts available.
+ */
+int pit_register_timeout(
+    uint32_t ms,
+    timeout_callback_t callback,
+    void* data
+);
+
+/**
+ * Cancels selected timeout by ID.
+ */
+void pit_cancel_timeout(int id);
 #endif
