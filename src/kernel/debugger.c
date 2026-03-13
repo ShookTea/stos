f40b815e6208e9d4ecaa10c8d149bb76ac06b172
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-13 16:12:20 +0100

    Support for backspace removing characters in the debug mode
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/kernel/debugger.c
+++ src/kernel/debugger.c
@@ -4,21 +4,41 @@
 #include <stdio.h>
 #include <kernel/tty.h>
 
-static char command_buffer[64];
+#define MAX_COMMAND_LENGTH 64
+
+static char command_buffer[MAX_COMMAND_LENGTH];
 static uint8_t command_length = 0;
 
 static void handle_key_event(keyboard_event_t evt)
 {
-    if (evt.pressed && evt.ascii) {
-        putchar(evt.ascii);
+    if (!evt.pressed) {
+        return;
     }
+    if (!evt.ascii) {
+        // TODO: handle cursor moving left and right
+        return;
+    }
+    if (evt.key_code == KCODE_BACKSPACE) {
+        if (command_length > 0) {
+            // Backspace moves cursor one step to the left, so sending space
+            // and another backspace effectively removes the character to the
+            // left.
+            tty_writestring("\b \b");
+            command_length--;
+        }
+    } else if (command_length < MAX_COMMAND_LENGTH) {
+        putchar(evt.ascii);
+        command_buffer[command_length] = evt.ascii;
+        command_length++;
+    }
+
 }
 void debugger_init()
 {
     keyboard_register_listener(handle_key_event);
     puts("");
     puts("");
-    puts("kernel debugger");
+    puts("Kernel debugger. Write \"help\" to get list of available commands.");
     printf("# ");
     tty_enable_cursor(0, 15);
 }
