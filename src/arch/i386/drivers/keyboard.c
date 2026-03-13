ff030fa4483dd2510343eea0eb4be2fc81e26451
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-13 11:15:53 +0100

    Mapping to keycodes
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/arch/i386/drivers/keyboard.c
+++ src/arch/i386/drivers/keyboard.c
@@ -87,15 +87,152 @@ static void buffer_dump_and_panic()
     abort();
 }
 
+/**
+ * Converts byte+extended+prinstscreen comination to KCODE_ values.
+ */
+static uint8_t get_key_code(
+    uint8_t byte,
+    bool extended,
+    bool printscreen
+) {
+    if (printscreen) return KCODE_PRNT_SCRN;
+    if (extended) {
+        switch (byte) {
+            case 0x11: return KCODE_RIGHT_ALT;
+            case 0x14: return KCODE_RIGHT_CTRL;
+            case 0x1F: return KCODE_LEFT_SYSTEM;
+            case 0x27: return KCODE_RIGHT_SYSTEM;
+            case 0x2F: return KCODE_APPS;
+            case 0x4A: return KCODE_NUMPAD_SLASH;
+            case 0x5A: return KCODE_NUMPAD_ENTER;
+            case 0x69: return KCODE_END;
+            case 0x6B: return KCODE_ARROW_LEFT;
+            case 0x6C: return KCODE_HOME;
+            case 0x70: return KCODE_INSERT;
+            case 0x71: return KCODE_DELETE;
+            case 0x72: return KCODE_ARROW_DOWN;
+            case 0x74: return KCODE_ARROW_RIGHT;
+            case 0x75: return KCODE_ARROW_UP;
+            case 0x7A: return KCODE_PAGE_DOWN;
+            case 0x7D: return KCODE_PAGE_UP;
+            default:
+                // TODO: handle it gracefully instead of panicking
+                printf("Unrecognized extended key code: %#02x\n", byte);
+                abort();
+        }
+    }
+    switch (byte) {
+        case 0x01: return KCODE_F9;
+        case 0x03: return KCODE_F5;
+        case 0x04: return KCODE_F3;
+        case 0x05: return KCODE_F1;
+        case 0x06: return KCODE_F2;
+        case 0x07: return KCODE_F12;
+        case 0x09: return KCODE_F10;
+        case 0x0A: return KCODE_F8;
+        case 0x0B: return KCODE_F6;
+        case 0x0C: return KCODE_F4;
+        case 0x0D: return KCODE_TAB;
+        case 0x0E: return KCODE_GRAVE_ACCENT;
+
+        case 0x11: return KCODE_LEFT_ALT;
+        case 0x12: return KCODE_LEFT_SHIFT;
+        case 0x14: return KCODE_LEFT_CTRL;
+        case 0x15: return KCODE_Q;
+        case 0x16: return KCODE_1;
+        case 0x1A: return KCODE_Z;
+        case 0x1B: return KCODE_S;
+        case 0x1C: return KCODE_A;
+        case 0x1D: return KCODE_W;
+        case 0x1E: return KCODE_2;
+
+        case 0x21: return KCODE_C;
+        case 0x22: return KCODE_X;
+        case 0x23: return KCODE_D;
+        case 0x24: return KCODE_E;
+        case 0x25: return KCODE_4;
+        case 0x26: return KCODE_3;
+        case 0x29: return KCODE_SPACE;
+        case 0x2A: return KCODE_V;
+        case 0x2B: return KCODE_F;
+        case 0x2C: return KCODE_T;
+        case 0x2D: return KCODE_R;
+        case 0x2E: return KCODE_5;
+
+        case 0x31: return KCODE_N;
+        case 0x32: return KCODE_B;
+        case 0x33: return KCODE_H;
+        case 0x34: return KCODE_G;
+        case 0x35: return KCODE_Y;
+        case 0x36: return KCODE_6;
+        case 0x3A: return KCODE_M;
+        case 0x3B: return KCODE_J;
+        case 0x3C: return KCODE_U;
+        case 0x3D: return KCODE_7;
+        case 0x3E: return KCODE_8;
+
+        case 0x41: return KCODE_COMMA;
+        case 0x42: return KCODE_K;
+        case 0x43: return KCODE_I;
+        case 0x44: return KCODE_O;
+        case 0x45: return KCODE_0;
+        case 0x46: return KCODE_9;
+        case 0x49: return KCODE_FULL_STOP;
+        case 0x4A: return KCODE_SLASH;
+        case 0x4B: return KCODE_L;
+        case 0x4C: return KCODE_SEMICOLON;
+        case 0x4D: return KCODE_P;
+        case 0x4E: return KCODE_MINUS;
+
+        case 0x52: return KCODE_APOSTROPHE;
+        case 0x54: return KCODE_LEFT_BRACKET;
+        case 0x55: return KCODE_EQUALS;
+        case 0x58: return KCODE_CAPS_LOCK;
+        case 0x59: return KCODE_RIGHT_SHIFT;
+        case 0x5A: return KCODE_ENTER;
+        case 0x5B: return KCODE_RIGHT_BRACKET;
+        case 0x5D: return KCODE_BACKSLASH;
+
+        case 0x66: return KCODE_BACKSPACE;
+        case 0x69: return KCODE_NUMPAD_1;
+        case 0x6B: return KCODE_NUMPAD_4;
+        case 0x6C: return KCODE_NUMPAD_7;
+
+        case 0x70: return KCODE_NUMPAD_0;
+        case 0x71: return KCODE_NUMPAD_DEL;
+        case 0x72: return KCODE_NUMPAD_2;
+        case 0x73: return KCODE_NUMPAD_5;
+        case 0x74: return KCODE_NUMPAD_6;
+        case 0x75: return KCODE_NUMPAD_8;
+        case 0x76: return KCODE_ESC;
+        case 0x77: return KCODE_NUM_LOCK;
+        case 0x78: return KCODE_F11;
+        case 0x79: return KCODE_NUMPAD_PLUS;
+        case 0x7A: return KCODE_NUMPAD_3;
+        case 0x7B: return KCODE_NUMPAD_MINUS;
+        case 0x7C: return KCODE_NUMPAD_STAR;
+        case 0x7D: return KCODE_NUMPAD_9;
+        case 0x7E: return KCODE_SCROLL_LOCK;
+
+        case 0x83: return KCODE_F7;
+        default:
+            // TODO: handle it gracefully instead of panicking
+            printf("Unrecognized extended key code: %#02x\n", byte);
+            abort();
+    }
+}
+
 static void key_handler(
     uint8_t byte,
     bool release,
     bool extended,
     bool printscreen
 ) {
+    uint8_t keycode = get_key_code(byte, extended, printscreen);
     printf(
-        "%10s %c%#02x%s\n",
+        "%10s %#02x - %c%#02x%s\n",
         release ? "released" : "pressed",
+        keycode,
         extended ? 'E' : 'S',
         byte,
         printscreen ? " (print screen)" : ""
--- src/include/kernel/drivers/keyboard.h
+++ src/include/kernel/drivers/keyboard.h
@@ -1,6 +1,140 @@
 #ifndef KERNEL_DRIVERS_KEYBOARD_H
 #define KERNEL_DRIVERS_KEYBOARD_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 void keyboard_init();
+
+typedef struct {
+    uint8_t key_code;
+    char ascii; // can be null if not applicable
+    bool pressed;
+    bool l_shift_pressed;
+    bool r_shift_pressed;
+    bool l_ctrl_pressed;
+    bool r_ctrl_pressed;
+    bool l_alt_pressed;
+    bool r_alt_pressed;
+    bool l_gui_pressed;
+    bool r_gui_pressed;
+    bool caps_lock_on;
+    bool num_lock_on;
+    bool scroll_lock_on;
+} keyboard_event_t;
+
+// 0b 000 00000
+// First three bits: row on a keyboard
+// Remaining bits: order on the row
+// Using a typical 104-key US QWERTY layout, treating multi-row keys (Enter,
+// numpad enter, numpad +) as if they were on the lower row.
+#define KCODE_ESC           ((0 << 5) | 0)
+#define KCODE_F1            ((0 << 5) | 1)
+#define KCODE_F2            ((0 << 5) | 2)
+#define KCODE_F3            ((0 << 5) | 3)
+#define KCODE_F4            ((0 << 5) | 4)
+#define KCODE_F5            ((0 << 5) | 5)
+#define KCODE_F6            ((0 << 5) | 6)
+#define KCODE_F7            ((0 << 5) | 7)
+#define KCODE_F8            ((0 << 5) | 8)
+#define KCODE_F9            ((0 << 5) | 9)
+#define KCODE_F10           ((0 << 5) | 10)
+#define KCODE_F11           ((0 << 5) | 11)
+#define KCODE_F12           ((0 << 5) | 12)
+#define KCODE_PRNT_SCRN     ((0 << 5) | 13)
+#define KCODE_SCROLL_LOCK   ((0 << 5) | 14)
+
+#define KCODE_GRAVE_ACCENT  ((1 << 5) | 0)
+#define KCODE_1             ((1 << 5) | 1)
+#define KCODE_2             ((1 << 5) | 2)
+#define KCODE_3             ((1 << 5) | 3)
+#define KCODE_4             ((1 << 5) | 4)
+#define KCODE_5             ((1 << 5) | 5)
+#define KCODE_6             ((1 << 5) | 6)
+#define KCODE_7             ((1 << 5) | 7)
+#define KCODE_8             ((1 << 5) | 8)
+#define KCODE_9             ((1 << 5) | 9)
+#define KCODE_0             ((1 << 5) | 10)
+#define KCODE_MINUS         ((1 << 5) | 11)
+#define KCODE_EQUALS        ((1 << 5) | 12)
+#define KCODE_BACKSLASH     ((1 << 5) | 13)
+#define KCODE_BACKSPACE     ((1 << 5) | 14)
+#define KCODE_INSERT        ((1 << 5) | 15)
+#define KCODE_HOME          ((1 << 5) | 16)
+#define KCODE_PAGE_UP       ((1 << 5) | 17)
+#define KCODE_NUM_LOCK      ((1 << 5) | 18)
+#define KCODE_NUMPAD_SLASH  ((1 << 5) | 19)
+#define KCODE_NUMPAD_STAR   ((1 << 5) | 20)
+#define KCODE_NUMPAD_MINUS  ((1 << 5) | 21)
+
+#define KCODE_TAB           ((2 << 5) | 0)
+#define KCODE_Q             ((2 << 5) | 1)
+#define KCODE_W             ((2 << 5) | 2)
+#define KCODE_E             ((2 << 5) | 3)
+#define KCODE_R             ((2 << 5) | 4)
+#define KCODE_T             ((2 << 5) | 5)
+#define KCODE_Y             ((2 << 5) | 6)
+#define KCODE_U             ((2 << 5) | 7)
+#define KCODE_I             ((2 << 5) | 8)
+#define KCODE_O             ((2 << 5) | 9)
+#define KCODE_P             ((2 << 5) | 10)
+#define KCODE_LEFT_BRACKET  ((2 << 5) | 11)
+#define KCODE_RIGHT_BRACKET ((2 << 5) | 12)
+#define KCODE_DELETE        ((2 << 5) | 13)
+#define KCODE_END           ((2 << 5) | 14)
+#define KCODE_PAGE_DOWN     ((2 << 5) | 15)
+#define KCODE_NUMPAD_7      ((2 << 5) | 16)
+#define KCODE_NUMPAD_8      ((2 << 5) | 17)
+#define KCODE_NUMPAD_9      ((2 << 5) | 18)
+
+#define KCODE_CAPS_LOCK     ((3 << 5) | 0)
+#define KCODE_A             ((3 << 5) | 1)
+#define KCODE_S             ((3 << 5) | 2)
+#define KCODE_D             ((3 << 5) | 3)
+#define KCODE_F             ((3 << 5) | 4)
+#define KCODE_G             ((3 << 5) | 5)
+#define KCODE_H             ((3 << 5) | 6)
+#define KCODE_J             ((3 << 5) | 7)
+#define KCODE_K             ((3 << 5) | 8)
+#define KCODE_L             ((3 << 5) | 9)
+#define KCODE_SEMICOLON     ((3 << 5) | 10)
+#define KCODE_APOSTROPHE    ((3 << 5) | 11)
+#define KCODE_ENTER         ((3 << 5) | 12)
+#define KCODE_NUMPAD_4      ((3 << 5) | 13)
+#define KCODE_NUMPAD_5      ((3 << 5) | 14)
+#define KCODE_NUMPAD_6      ((3 << 5) | 15)
+#define KCODE_NUMPAD_PLUS   ((3 << 5) | 20)
+
+#define KCODE_LEFT_SHIFT    ((4 << 5) | 0)
+#define KCODE_Z             ((4 << 5) | 1)
+#define KCODE_X             ((4 << 5) | 2)
+#define KCODE_C             ((4 << 5) | 3)
+#define KCODE_V             ((4 << 5) | 4)
+#define KCODE_B             ((4 << 5) | 5)
+#define KCODE_N             ((4 << 5) | 6)
+#define KCODE_M             ((4 << 5) | 7)
+#define KCODE_COMMA         ((4 << 5) | 8)
+#define KCODE_FULL_STOP     ((4 << 5) | 9)
+#define KCODE_SLASH         ((4 << 5) | 10)
+#define KCODE_RIGHT_SHIFT   ((4 << 5) | 11)
+#define KCODE_ARROW_UP      ((4 << 5) | 12)
+#define KCODE_NUMPAD_1      ((4 << 5) | 13)
+#define KCODE_NUMPAD_2      ((4 << 5) | 14)
+#define KCODE_NUMPAD_3      ((4 << 5) | 15)
+
+#define KCODE_LEFT_CTRL     ((5 << 5) | 0)
+#define KCODE_LEFT_SYSTEM   ((5 << 5) | 1)
+#define KCODE_LEFT_ALT      ((5 << 5) | 2)
+#define KCODE_SPACE         ((5 << 5) | 3)
+#define KCODE_RIGHT_ALT     ((5 << 5) | 4)
+#define KCODE_APPS          ((5 << 5) | 5)
+#define KCODE_RIGHT_SYSTEM  ((5 << 5) | 6)
+#define KCODE_RIGHT_CTRL    ((5 << 5) | 7)
+#define KCODE_ARROW_LEFT    ((5 << 5) | 8)
+#define KCODE_ARROW_DOWN    ((5 << 5) | 9)
+#define KCODE_ARROW_RIGHT   ((5 << 5) | 10)
+#define KCODE_NUMPAD_0      ((5 << 5) | 11)
+#define KCODE_NUMPAD_DEL    ((5 << 5) | 12)
+#define KCODE_NUMPAD_ENTER  ((5 << 5) | 13)
 
 #endif
