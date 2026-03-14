47ba9320b4e6c81b1e5ed3cd308673e46f030a30
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-14 10:59:46 +0100

    Gathering ACPI SDT entries
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/arch/i386/acpi.c
+++ src/arch/i386/acpi.c
@@ -1,10 +1,31 @@
+#include "stdlib.h"
 #include <kernel/acpi.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-static uint32_t rsdt_physical_address = 0;
+static void load_rsdt_pointer(uint32_t pointer)
+{
+    acpi_sdt_header_t* header = (acpi_sdt_header_t*)pointer;
+    if (strncmp(header->signature, "RSDT", 0) != 0) {
+        puts("Invalid RSDT signature");
+        return;
+    }
+    // TODO: calculate checksum - https://wiki.osdev.org/RSDT
+    acpi_rsdt_t* rsdt = (acpi_rsdt_t*)header;
+    uint8_t entries = (rsdt->header.length - sizeof(acpi_sdt_header_t)) / 4;
+    for (uint8_t i = 0; i < entries; i++) {
+        uint32_t sdt_pointer = rsdt->pointers_to_ther_sdt[i];
+        acpi_sdt_header_t* sdt_header = (acpi_sdt_header_t*)sdt_pointer;
+        putchar(sdt_header->signature[0]);
+        putchar(sdt_header->signature[1]);
+        putchar(sdt_header->signature[2]);
+        putchar(sdt_header->signature[3]);
+        puts("");
+    }
+    abort();
+}
 
 void acpi_init_old(rsdp_old_t rsdp)
 {
@@ -32,8 +53,7 @@ void acpi_init_old(rsdp_old_t rsdp)
         puts("Invalid RSDPv1: wrong checksum");
         return;
     }
-
-
+    load_rsdt_pointer(rsdp.rsdt_address);
 }
 
 void acpi_init_new(rsdp_new_t rsdp)
@@ -88,6 +108,5 @@ void acpi_init_new(rsdp_new_t rsdp)
         return;
     }
 
-    // TODO: in 64-bit mode this will contain a full 64-bit address
-    rsdt_physical_address = (uint32_t)(rsdp.rsdt_address & 0xFFFFFFFF);
+    puts("RSDPv2 valid, but 64-bit mode not implemented yet.");
 }
--- src/include/kernel/acpi.h
+++ src/include/kernel/acpi.h
@@ -24,6 +24,23 @@ typedef struct {
     uint8_t reserved[3];
 } __attribute__((packed)) rsdp_new_t;
 
+typedef struct {
+    char signature[4];
+    uint32_t length;
+    uint8_t revision;
+    uint8_t checksum;
+    char oemid[6];
+    char oem_table_id[8];
+    uint32_t oem_revision;
+    uint32_t creator_id;
+    uint32_t creator_revision;
+} acpi_sdt_header_t;
+
+typedef struct {
+    acpi_sdt_header_t header;
+    uint32_t pointers_to_ther_sdt[0];
+} acpi_rsdt_t;
+
 void acpi_init_old(rsdp_old_t);
 void acpi_init_new(rsdp_new_t);
 
