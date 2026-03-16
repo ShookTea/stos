ee5050344dbdd379f9394379ab5bb472eb65bbf8
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-16 19:23:11 +0100

    Iterating over files in TAR
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/kernel/vfs/initrd.c
+++ src/kernel/vfs/initrd.c
@@ -1,3 +1,4 @@
+#include "kernel/memory/pmm.h"
 #include "kernel/multiboot2.h"
 #include "kernel/paging.h"
 #include <kernel/vfs/initrd.h>
@@ -9,6 +10,35 @@
 
 static vfs_node_t* initrd = NULL;
 static bool mounted = false;
+static char empty_name[100];
+
+/**
+ * Iterates over file in TAR. Loads next TAR header to `next`.
+ */
+static void initrd_load_tar(tar_header_t* tar_header, tar_header_t** next)
+{
+    if (memcmp(tar_header->filename, empty_name, 100) == 0)
+    {
+        *next = NULL;
+        return;
+    }
+
+    tar_header->filename[99] = '\0';
+    uint32_t size_bytes = strtol(tar_header->size, NULL, 8);
+    puts(tar_header->filename);
+    printf("Size: %d B\n", size_bytes);
+
+    char entry[16];
+    strncpy(entry, ((char*)tar_header) + 512, 16);
+    entry[15] = 0;
+    puts(entry);
+
+    uint32_t size_aligned = size_bytes;
+    if (size_aligned % 512 != 0) {
+        size_aligned += 512 - (size_aligned % 512);
+    }
+    *next = (tar_header_t*)(((char*)tar_header) + 512 + size_aligned);
+}
 
 vfs_node_t* initrd_mount()
 {
@@ -17,6 +47,7 @@ vfs_node_t* initrd_mount()
     }
     mounted = true;
     puts("Loading initrd from memory");
+    memset(empty_name, 0, 100);
 
     multiboot_tag_boot_module_t* initrd_module = NULL;
     for (uint32_t i = 0; i < multiboot2_get_modules_count(); i++) {
@@ -35,10 +66,9 @@ vfs_node_t* initrd_mount()
         initrd_module->module_phys_addr_start
     );
 
-    tar_header->filename[99] = '\0';
-    uint32_t size_bytes = strtol(tar_header->size, NULL, 8);
-    puts(tar_header->filename);
-    printf("Size: %d B\n", size_bytes);
+    do {
+        initrd_load_tar(tar_header, &tar_header);
+    } while (tar_header != NULL);
 
     return initrd;
 }
