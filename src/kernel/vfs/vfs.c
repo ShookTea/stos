5997969c4f0aa2b90b5cdd582451434489af9108
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-18 17:40:01 +0100

    Implementing functions for VFS root node
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/kernel/vfs/vfs.c
+++ src/kernel/vfs/vfs.c
@@ -12,6 +12,30 @@ vfs_node_t* vfs_root = 0;
 vfs_node_t** mounted_nodes = NULL;
 uint32_t mounted_notes_count = 0;
 
+static struct dirent* vfs_root_readdir(
+    __attribute__((unused))struct vfs_node* node,
+    size_t index
+) {
+    if (index > mounted_notes_count) {
+        return NULL;
+    }
+    static struct dirent ent;
+    strcpy(ent.name, mounted_nodes[index]->filename);
+    return &ent;
+}
+
+static vfs_node_t* vfs_root_finddir(
+    __attribute__((unused))struct vfs_node* node,
+    char* name
+) {
+    for (size_t i = 0; i < mounted_notes_count; i++) {
+        if (strcmp(mounted_nodes[i]->filename, name) == 0) {
+            return mounted_nodes[i];
+        }
+    }
+    return NULL;
+}
+
 void vfs_init()
 {
     vfs_root = kmalloc(sizeof(vfs_node_t));
@@ -23,8 +47,8 @@ void vfs_init()
     vfs_root->close_node = NULL;
     vfs_root->read_node = NULL;
     vfs_root->write_node = NULL;
-    vfs_root->readdir_node = NULL;
-    vfs_root->finddir_node = NULL;
+    vfs_root->readdir_node = vfs_root_readdir;
+    vfs_root->finddir_node = vfs_root_finddir;
 
     mounted_nodes = kmalloc(sizeof(vfs_node_t*) * VFS_MAX_MOUNTED_NODES);
 }
