7afc020c02ee051bdff785b4921f5f45225fc65c
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-22 16:16:38 +0100

    Create scheduler.h
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

---
+++ src/kernel/task/scheduler.h
@@ -0,0 +1,23 @@
+#ifndef KERNEL_TASK_SCHEDULER_H
+#define KERNEL_TASK_SCHEDULER_H
+
+#include "task.h"
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct {
+    /** Total number of tasks */
+    size_t num_tasks;
+    /** Tasks in TASK_WAITING state */
+    size_t num_waiting;
+    /** Tasks in TASK_BLOCKED state */
+    size_t num_blocked;
+    /** Tasks in TASK_SLEEPING state */
+    size_t num_sleeping;
+    /** Time spent in idle task */
+    uint64_t total_idle_time;
+    /** Currently running task */
+    task_t* current_task;
+} scheduler_stats_t;
+
+#endif
