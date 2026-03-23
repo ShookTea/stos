4f04e33d36fa33eef4439fa8dc6c96aa611c5a52
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-23 15:14:21 +0100

    Reordering of registers in registers_t
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- src/arch/i386/idt/idt.h
+++ src/arch/i386/idt/idt.h
@@ -62,8 +62,8 @@ typedef struct
 
 typedef struct
 {
+    uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi; // Pushed by pusha
     uint32_t gs, fs, es, ds; // Segment registers
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
     uint32_t int_no; // Interrupt number
     uint32_t err_code; // Error code (if applicable)
     uint32_t eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
