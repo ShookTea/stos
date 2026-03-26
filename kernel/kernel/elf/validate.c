e6cab067e2ffe5135c5f262ec4458618831f9aa8
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-26 17:11:33 +0100

    Validation of architecture
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- kernel/kernel/elf/validate.c
+++ kernel/kernel/elf/validate.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-
 bool elf_validate(void* addr)
 {
     elf_header_32bit_t* header = addr;
@@ -13,12 +12,29 @@ bool elf_validate(void* addr)
         || header->magic[3] != 'F'
     ) {
         printf(
-            "Validation failed: wrong magic value (%#2x%2x%2x%2x)\n",
+            "Invalid ELF: wrong magic value (%#2x%2x%2x%2x)\n",
             header->magic[0],
             header->magic[1],
             header->magic[2],
             header->magic[3]
         );
+        return false;
+    }
+
+    if (header->class != 1) {
+        puts("Invalid ELF: only 32-bit apps are supported.");
+        return false;
+    }
+    if (header->endianness != 1) {
+        puts("Invalid ELF: only little endian apps are supported.");
+        return false;
+    }
+    if (header->version != 1) {
+        puts("Invalid ELF: only version 1 supported");
+        return false;
+    }
+    if (header->machine != ELF_OBJ_MACHINE_X86) {
+        puts("Invalid ELF: requires x86 architecture");
         return false;
     }
 
