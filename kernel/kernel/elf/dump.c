fd512ccc9d4ab08b54297407caebfe52b90459fa
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-27 17:16:02 +0100

    Refactor
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

---
+++ kernel/kernel/elf/dump.c
@@ -0,0 +1,13 @@
+#include <kernel/memory/kmalloc.h>
+#include <kernel/vfs/vfs.h>
+#include <kernel/elf.h>
+
+void elf_dump(vfs_file_t* handle)
+{
+    void* file = kmalloc_flags(handle->node->length, KMALLOC_ZERO);
+    vfs_read(handle, handle->node->length, file);
+    vfs_close(handle);
+    // For now the validation code prints all the debug info
+    elf_validate(file);
+    kfree(file);
+}
