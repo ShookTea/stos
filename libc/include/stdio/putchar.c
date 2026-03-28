119d3dd82166d9f008883a67de6aabb0a7589fef
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-28 15:50:07 +0100

    putchar now works in libc as well
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- libc/include/stdio/putchar.c
+++ libc/include/stdio/putchar.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 #if defined(__is_libk)
-#include <kernel/terminal.h>
+    #include <kernel/terminal.h>
+#else
+    #include <unistd.h>
 #endif
 
 int putchar(int ic)
 {
-    #if defined(__is_libk)
     char c = (char) ic;
-    terminal_write(&c, sizeof(c));
-    return ic;
+    #if defined(__is_libk)
+        terminal_write(&c, sizeof(c));
     #else
-    // TODO: implement stdio for userspace and write system call
-    return EOF;
+        write(1, &c, 1);
     #endif
+    return ic;
 }
