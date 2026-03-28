41650f41ca5d352d6ef71695891ce79c8b63ec3b
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-28 14:54:54 +0100

    Better implementation of crt0.c - support for C++ in the future, plus parsing arguments
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- usermode/crt0.c
+++ usermode/crt0.c
@@ -1,12 +1,20 @@
-extern int main(void);
+extern int main(int argc, char** argv, char** envp);
+
+#ifdef __cplusplus
+// These are provided by the linker
+extern void (*__init_array_start[])(void);
+extern void (*__init_array_end[])(void);
+extern void (*__fini_array_start[])(void);
+extern void (*__fini_array_end[])(void);
+#endif
 
 /**
  * This file is an actual entrypoint for all usermode programs. The _start is
  * what's actually getting executed when running the app.
  *
  * It's responsible for things like:
- * - Setting up argc, argv, envp from stack
  * - Initializing libc
+ * - Setting up argc, argv, envp from stack
  * - Calling constructors
  * - Calling main()
  * - Calling exit()
@@ -14,7 +22,34 @@ extern int main(void);
  */
 __attribute__((section(".text._start")))
 void _start(void) {
-    int ret = main();
+    // 1. Initialize libc data structures (so far there's nothing needed)
+
+    #ifdef __cplusplus
+    // 2. Call global constructors (C++)
+    int init_count = __init_array_end - __init_array_start;
+    for (int i = 0; i < init_count; i++) {
+        __init_array_start[i]();
+    }
+    #endif
+
+    // 3. Parse arguments/env from stack
+    // TODO: that's not implemented yet, empty values used instead
+    int argc = 0;
+    char** argv = 0;
+    char** envp = 0;
+
+    // 4. Call main
+    int ret = main(argc, argv, envp);
+
+    #ifdef __cplusplus
+    // 5. Call global destructors (C++)
+    int fini_count = __fini_array_end - __fini_array_start;
+    for (int i = fini_count - 1; i >= 0; i--) {
+        __fini_array_start[i]();
+    }
+    #endif
+
+    // 6. Exit with status code
 
     // Syscall: exit(ret)
     // Syscall number 1 (SYS_EXIT) in EAX, status code in ECX
@@ -27,5 +62,5 @@ void _start(void) {
         : "eax", "ecx"     // clobbered registers
     );
 
-    while(1); // hang for now
+    __builtin_unreachable();
 }
