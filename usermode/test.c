0867224c0e4b643066ce949b495e20e0770314cb
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-29 13:01:12 +0200

    More testing of malloc
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- usermode/test.c
+++ usermode/test.c
@@ -4,17 +4,21 @@
 int main(void)
 {
     puts("Hello from userspace!");
-    void* array1 = malloc(sizeof(int) * 16);
-    void* array2 = malloc(sizeof(int) * 32);
-    void* array3 = malloc(sizeof(int) * 16);
+    void* array1 = malloc(sizeof(int) * 1024);
+    void* array2 = malloc(sizeof(int) * 1024);
+    void* array3 = malloc(sizeof(int) * 1024);
 
     printf("Addr array1 = %#x\n", array1);
     printf("Addr array2 = %#x\n", array2);
     printf("Addr array3 = %#x\n", array3);
 
     ((int*)array1)[5] = 32;
+    ((int*)array2)[5] = 64;
+    ((int*)array3)[5] = 128;
 
     printf("array1 val = %d\n", ((int*)array1)[5]);
+    printf("array2 val = %d\n", ((int*)array2)[5]);
+    printf("array3 val = %d\n", ((int*)array3)[5]);
 
     return 0;
 }
