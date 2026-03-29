1fdebd524ce0d7a15451d73603774bbcb703448f
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-03-29 11:24:31 +0200

    Testing brk syscall
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- usermode/test.c
+++ usermode/test.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char* data = "Hello, world!\n";
-
-// Helper to print a null-terminated string
-static void print(const char* str)
-{
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    write(1, str, len);
-}
-
 int main(void)
 {
-    print("Hello from userspace!\n");
-    print("This is a test program.\n");
-    print(data);
-    print("Holy shit.\n");
-    puts("Foo");
+    puts("Hello from userspace!");
+    void* heap_end = brk(0);
+    printf("Current heap end: %#x\n", heap_end);
+    heap_end = brk(heap_end + 10000);
+    printf("Heap end after increment: %#x\n", heap_end);
+    heap_end = brk(0);
+    printf("Current heap end: %#x\n", heap_end);
+    heap_end = brk(heap_end - 5000);
+    printf("Heap end after decrement: %#x\n", heap_end);
+    heap_end = brk(0);
+    printf("Current heap end: %#x\n", heap_end);
 
     return 0;
 }
