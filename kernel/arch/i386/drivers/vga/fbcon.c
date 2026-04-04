69bcff9332983090ecf04d2eddc3596517c9326c
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-04-04 20:01:08 +0200

    Basics for blinking
    Signed-off-by: Norbert Kowalik <norbert.kowalik@icloud.com>

--- kernel/arch/i386/drivers/vga/fbcon.c
+++ kernel/arch/i386/drivers/vga/fbcon.c
@@ -1,18 +1,28 @@
 #include "kernel/drivers/vga/fbcon.h"
+#include "kernel/drivers/pit.h"
 #include "kernel/drivers/vga/font.h"
 #include "kernel/drivers/vga/rgb.h"
 #include "kernel/memory/kmalloc.h"
 #include <stdint.h>
 
+#define CURSOR_BLINK_INTERVAL_MS 100
+
 uint32_t* char_buffer;
 size_t cursor_x = 0;
 size_t cursor_y = 0;
+
+static void fbcon_cursor_blink()
+{
+    // Re-add handler to run this in interval
+    pit_register_timeout(CURSOR_BLINK_INTERVAL_MS, fbcon_cursor_blink, NULL);
+}
 
 void fbcon_init()
 {
     char_buffer = kmalloc(
         sizeof(uint32_t) * fbcon_get_columns() * fbcon_get_rows()
     );
+    pit_register_timeout(CURSOR_BLINK_INTERVAL_MS, fbcon_cursor_blink, NULL);
 }
 
 void fbcon_putentryat(uint32_t c, uint32_t fg, uint32_t bg, size_t x, size_t y)
