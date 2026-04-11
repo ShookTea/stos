336f3edb2feb2cd90e8e509f4d350e6cc7eb71dd
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-04-11 12:21:01 +0200

    Metadata allocation

--- kernel/kernel/vfs/device/hd.c
+++ kernel/kernel/vfs/device/hd.c
@@ -2,10 +2,18 @@
 #include "kernel/debug.h"
 #include "kernel/drivers/ata.h"
 #include "kernel/memory/kmalloc.h"
+#include "kernel/task/wait.h"
 #include "kernel/vfs/vfs.h"
 
 #define _debug_puts(...) debug_puts_c("VFS/dev/hd", __VA_ARGS__)
 #define _debug_printf(...) debug_printf_c("VFS/dev/hd", __VA_ARGS__)
+
+typedef struct {
+    // one of ATA_DRIVE_ values
+    uint8_t disk_id;
+    // Tasks queue
+    wait_obj_t* wait_obj;
+} hd_metadata_t;
 
 static vfs_node_t** nodes = NULL;
 
@@ -35,6 +43,13 @@ vfs_node_t** device_hd_mount()
 
         vfs_node_t* node = kmalloc_flags(sizeof(vfs_node_t), KMALLOC_ZERO);
         vfs_populate_node(node, drive_name, VFS_TYPE_BLOCK_DEVICE);
+        hd_metadata_t* metadata = kmalloc_flags(
+            sizeof(hd_metadata_t),
+            KMALLOC_ZERO
+        );
+        node->metadata = metadata;
+        metadata->disk_id = *ata_drive_ptr;
+        metadata->wait_obj = wait_allocate_queue();
         nodes[pointer_index] = node;
 
         drive_letter++;
@@ -50,7 +65,15 @@ void device_hd_unmount()
     if (nodes != NULL) {
         vfs_node_t** ptr = nodes;
         while (*ptr != NULL) {
-            kfree(*ptr);
+            vfs_node_t* node = *ptr;
+            hd_metadata_t* metadata = node->metadata;
+            if (metadata != NULL) {
+                if (metadata->wait_obj != NULL) {
+                    kfree(metadata->wait_obj);
+                }
+                kfree(metadata);
+            }
+            kfree(node);
             ptr++;
         }
         kfree(nodes);
