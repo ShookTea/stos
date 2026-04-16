ce9d2b06585d6c64bff9f190e2dcd140cc1eeaf0
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-04-16 19:47:54 +0200

    Test program for experimenting with TTY attributes

--- usermode/read_tty.c
+++ usermode/read_tty.c
@@ -1,3 +1,4 @@
+#include <termios.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,13 +8,42 @@
 int main(void)
 {
     int fd = open("/dev/tty", O_RDONLY);
-    printf("FD = %d\n", fd);
+    if (fd == -1) {
+        puts("/dev/tty file not found");
+    }
+
     uint8_t* data = malloc(32);
+
+    struct termios termios;
+    tcgetattr(fd, &termios);
+    uint32_t original_lflag = termios.c_lflag; // Keep for later recovery
+
+    // Set the ECHO flag on
+    termios.c_lflag |= ECHO;
+    tcsetattr(fd, TCSANOW, &termios);
+
+    printf("Input (visible): ");
     read(fd, data, 32);
     data[31] = 0;
     char* text = (char*)data;
-    printf(">%s<\n", text);
+    printf("Result: %s", text);
+
+    // Set the ECHO flag off
+    termios.c_lflag &= ~ECHO;
+    tcsetattr(fd, TCSANOW, &termios);
+
+    printf("Input (hidden): ");
+    read(fd, data, 32);
+    data[31] = 0;
+    text = (char*)data;
+    printf("\nResult: %s", text);
+
+    // Restore lflag
+    termios.c_lflag = original_lflag;
+    tcsetattr(fd, TCSANOW, &termios);
+
     close(fd);
+    free(data);
 
     return 0;
 }
