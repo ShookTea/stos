448bb0d8179c322c486cb3df038da8aa9fbd4917
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-04-22 18:34:01 +0200

    Color-coded categories

--- kernel/kernel/debug.c
+++ kernel/kernel/debug.c
@@ -5,6 +5,17 @@
 
 #if (KERNEL_DEBUG_ANY)
 
+static inline int cat_color(const char* cat)
+{
+    int i = 0;
+    char* t = (char*)cat;
+    while (*t != 0) {
+        i += *t;
+        t++;
+    }
+    return (i % 8) + 30;
+}
+
 void debug_puts(const char* string)
 {
     #if KERNEL_DEBUG_COM && !KERNEL_DEBUG_TERMINAL
@@ -17,7 +28,7 @@ void debug_puts(const char* string)
 
 void debug_puts_c(const char* cat, const char* string)
 {
-    debug_printf("\033[32;1m[%s]\033[0m %s\n", cat, string);
+    debug_printf("\033[%d;1m[%s]\033[0m %s\n", cat_color(cat), cat, string);
 }
 
 void debug_printf(const char* format, ...)
@@ -42,7 +53,7 @@ void debug_printf_c(const char* cat, const char* format, ...)
 {
     #if KERNEL_DEBUG_COM && !KERNEL_DEBUG_TERMINAL
         char buffer[200];
-        snprintf(buffer, 200, "\033[32;1m[%s]\033[0m ", cat);
+        snprintf(buffer, 200, "\033[%d;1m[%s]\033[0m ", cat_color(cat), cat);
         char* string = buffer;
         for (;*string;string++) serial_put_c(*string);
         memset(buffer, 0, 200);
@@ -54,7 +65,7 @@ void debug_printf_c(const char* cat, const char* format, ...)
         string = buffer;
         for (;*string;string++) serial_put_c(*string);
     #elif KERNEL_DEBUG_TERMINAL
-        printf("\033[32;1m[%s]\033[0m ", cat);
+        printf("\033[%d;1m[%s]\033[0m ", cat_color(cat), cat);
         va_list args;
         va_start(args, format);
         int res = vprintf(format, args);
