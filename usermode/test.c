9ff8ef9564369965560f1a155f1606fb92376c3b
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-05-14 09:46:04 +0200

    Testing syscall

--- usermode/test.c
+++ usermode/test.c
@@ -1,19 +1,10 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <time.h>
 
 int main(void)
 {
     puts("Hello from userspace!");
-    struct timespec* sleep = malloc(sizeof(struct timespec));
-    sleep->tv_spec = 5;
-    sleep->tv_nsec = 0;
-    puts("Before sleep");
-    nanosleep(sleep, NULL);
-    puts("After sleep");
-
+    time_t ts = time(NULL);
+    printf("timestamp = %dll\n", ts);
     return 0;
 }
