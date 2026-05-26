75ef5efb36d3384648d94ca36bca4024a1049113
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-05-26 15:47:35 +0200

    Implementation of dup

--- kernel/kernel/task/syscall/dup.c
+++ kernel/kernel/task/syscall/dup.c
@@ -3,6 +3,61 @@
 #include "kernel/task/syscall.h"
 #include "kernel/task/task.h"
 
+/**
+ * Handle case where user didn't specify newfd
+ */
+static int sys_dup_no_newfd(
+    task_t* current,
+    task_file_descriptor_t* fd_obj
+) {
+    // Duplicate file descriptor
+    task_file_descriptor_t* fd_new_obj = task_add_fd(current, fd_obj->file);
+    if (fd_new_obj == NULL) {
+        return -EIO;
+    }
+
+    return fd_new_obj->identifier;
+}
+
+/**
+ * Handle case where user specified newfd
+ */
+static int sys_dup_with_newfd(
+    task_t* current,
+    task_file_descriptor_t* fd_obj,
+    int newfd
+) {
+    // First, try to search for existing instance with `newfd`
+    task_file_descriptor_t* existing_newfd = NULL;
+    for (size_t i = 0; i < current->fd_count; i++) {
+        task_file_descriptor_t* tmp = current->fd[i];
+        if (tmp->identifier == (uint32_t)newfd) {
+            existing_newfd = tmp;
+            break;
+        }
+    }
+
+    if (existing_newfd == NULL) {
+        // Given newfd identifier was never opened - just clone a descriptor
+        // and update it's identifier
+        task_file_descriptor_t* fd_new_obj = task_add_fd(current, fd_obj->file);
+        if (fd_new_obj == NULL) {
+            return -EIO;
+        }
+        fd_new_obj->identifier = (uint32_t)newfd;
+        return newfd;
+    }
+
+    if (existing_newfd->file != NULL) {
+        // The FD exists and is used right now. We need to close it first.
+        vfs_close(existing_newfd->file);
+    }
+
+    // Now we can reuse reuse it
+    existing_newfd->file = fd_obj->file;
+    return newfd;
+}
+
 int sys_dup(int oldfd, int newfd, int flags)
 {
     if (flags != 0 || oldfd < 0) {
@@ -14,9 +69,14 @@ int sys_dup(int oldfd, int newfd, int flags)
         return -ENOTSUP;
     }
 
-    return -ENOTSUP;
+    task_file_descriptor_t* fd_obj = syscall_get_descriptor_by_fd(oldfd);
+    if (fd_obj == NULL) {
+        return -EBADF;
+    }
 
     if (newfd == -1) {
-        // Just clone
+        return sys_dup_no_newfd(current, fd_obj);
     }
+
+    return sys_dup_with_newfd(current, fd_obj, newfd);
 }
