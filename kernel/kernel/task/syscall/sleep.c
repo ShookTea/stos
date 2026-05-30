293fa5847a023abfc6251f5f59a89f7b2af5d0e9
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-05-30 15:59:22 +0200

    SLEEP syscall now correctly handles signals

--- kernel/kernel/task/syscall/sleep.c
+++ kernel/kernel/task/syscall/sleep.c
@@ -1,19 +1,27 @@
 #include "kernel/task/syscall.h"
 #include <errno.h>
+#include <time.h>
 #include "kernel/task/scheduler.h"
 #include "kernel/task/wait.h"
 
-// TODO: If the call is interrupted by a signal handler, it should return -1,
-// set a correct error code, and store remaining time into `rem` (if not NULL).
-int sys_sleep(
-    const struct timespec* duration,
-    struct timespec* rem __attribute__((unused))
-) {
+int sys_sleep(const struct timespec* duration, struct timespec* rem)
+{
     task_t* current = scheduler_get_current_task();
     if (current == NULL) {
         return -ENOTSUP;
     }
 
-    wait_sleep((duration->tv_nsec / 1000) + (duration->tv_spec * 1000));
+    size_t millis =
+        (size_t)(duration->tv_spec * 1000)
+        + (size_t)(duration->tv_nsec / 1000000);
+    size_t remaining_ms = wait_sleep(millis);
+
+    if (remaining_ms > 0) {
+        if (rem != NULL) {
+            rem->tv_spec = (time_t)(remaining_ms / 1000);
+            rem->tv_nsec = (long)((remaining_ms % 1000) * 1000000);
+        }
+        return -EINTR;
+    }
     return 0;
 }
--- libc/include/errno.h
+++ libc/include/errno.h
@@ -11,6 +11,7 @@ typedef enum {
     EFAULT, // Bad address
     EINVAL, // Invalid argument
     EIO, // Input/output error
+    EINTR, // Interrupted by signal
     EISDIR, // File descriptor points to a directory
     ELOOP, // Too many levels of symbolic links
     ENOENT, // No such file or directory
