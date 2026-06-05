9f2eda5df9e9b34adc1f9202cfe993122d02b912
Norbert Kowalik <norbert.kowalik@icloud.com>
2026-06-05 17:19:42 +0200

    Move const definitions to sys/stat.h

--- libc/include/sys/stat.h
+++ libc/include/sys/stat.h
@@ -22,6 +22,33 @@ struct stat {
     struct timespec st_ctim; // Creation time
 };
 
+/** Read, write, execute/search by owner */
+#define S_IRWXU 0x01C0
+/** Read permission, owner */
+#define S_IRUSR 0x0100
+/** Write permission, owner */
+#define S_IWUSR 0x0080
+/** Execute/search permission, owner */
+#define S_IXUSR 0x0040
+
+/** Read, write, execute/search by group */
+#define S_IRWXG 0x0038
+/** Read permission, group */
+#define S_IRGRP 0x0020
+/** Write permission, group */
+#define S_IWGRP 0x0010
+/** Execute/search permission, group */
+#define S_IXGRP 0x0008
+
+/** Read, write, execute/search by others */
+#define S_IRWXO 0x0007
+/** Read permission, others */
+#define S_IROTH 0x0004
+/** Write permission, others */
+#define S_IWOTH 0x0002
+/** Execute/search permission, others */
+#define S_IXOTH 0x0001
+
 #ifdef __cplusplus
 extern "C" {
 #endif
